96b40d106389556df33d0d0f7dfd82a589480ccb
Naufattah <162897424+Aisgege@users.noreply.github.com>
2024-06-06 17:55:13 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,41 +1,33 @@
-const config = require('./config.js')
-const { Client, GatewayIntentBits, ActivityType, TextChannel } = require('discord.js');
+const config = require('./config.js');
+const { Client, GatewayIntentBits, ActivityType } = require('discord.js');
 const client = new Client({
   intents: Object.keys(GatewayIntentBits).map((i) => {
     return GatewayIntentBits[i];
   }),
 });
 
-/**
- * DISCORD : aisgege
- * BY AISGEGE 
- * SILAHKAN PAKAI SEPUASNYA
- */
-
 let statusIndex = 0;
 
 function update() {
-  let currentStatus = config.statusMessage[statusIndex];
-  if(!currentStatus) {
-    currentStatus = config.statusMessage[0]
-    statusIndex = 0
-  }
-
+  const currentActivityName = config.statusMessage[statusIndex % config.statusMessage.length];
   client.user.setPresence({
-    activities: [{ name: currentStatus, type: ActivityType.Custom}],
+    activities: [{ name: currentActivityName, type: ActivityType.Custom }],
     status: config.status,
   });
-
-  statusIndex += 1
+  statusIndex++;
 }
 
 client.once('ready', () => {
   console.log(`✅ ${client.user.tag} sudah siap!`);
   update();
 
-  setInterval(() => {
-      update()
-  }, config.statusInterval);
+  if (typeof config.statusInterval === 'number' && config.statusInterval > 0) {
+    setInterval(update, config.statusInterval);
+  } else {
+    console.error('Invalid statusInterval in config');
+  }
 });
 
-client.login(config.token);
+client.login(config.token).catch((error) => {
+  console.error('Error logging in to Discord API:', error);
+});
